8b8cf92f5c4c242ac91d15cbd50114eb648f70f4
if110013 <david.haunschmied@hotmail.com>
2015-12-09 18:04:58 +0100

    test

--- src/android/org/nfc/plugin/NfcTechPlugin.java
+++ src/android/org/nfc/plugin/NfcTechPlugin.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
-import org.apache.dordova.PluginResult;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
