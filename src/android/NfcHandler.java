19717741544d92d470a2f784a1c0e731fdf2875e
if110013 <david.haunschmied@hotmail.com>
2016-03-11 17:39:00 +0100

    test

--- src/android/NfcHandler.java
+++ src/android/NfcHandler.java
@@ -75,8 +75,8 @@ public class NfcHandler {
 		if(nfcvTag!=null){
 			try {
 				nfcvTag.connect();
-				int offset = 0;  // offset of first block to read
-				int blocks = 7;  // number of blocks to read
+				int offset = 1;  // offset of first block to read
+				int blocks = 8;  // number of blocks to read
 				byte[] cmd = new byte[]{
 						(byte)0x60,                  // flags: addressed (= UID field present)
 						(byte)0x23,                  // command: READ MULTIPLE BLOCKS
