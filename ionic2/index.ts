6e3bb76dff1a70a693f5a0d36175b21822f3c440
Valentin Iljaž <valentin@progmbh.com>
2017-03-29 18:02:46 +0200

    Add tsc

--- ionic2/index.ts
+++ ionic2/index.ts
@@ -1,7 +1,1 @@
-import {NgModule} from '@angular/core';
-import {NfcvService} from "./src/nfcv.service.ts";
-
-@NgModule({
-    providers: [NfcvService]
-})
-export class NfcvModule { }
+export {NfcvService} from "./src/nfcv.service";
